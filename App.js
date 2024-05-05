ff74312961fe9ad4bc7ed12e90d85d2fbdb5d158
iamab8 <anuragbhardwaj3108@gmail.com>
2024-05-05 11:08:14 +0530

    5May commit

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Login from "./App/Screens/LoginScreen/Login";
+import Colors from "./App/Utils/Colors";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+import { NavigationContainer } from "@react-navigation/native";
+import TabNavigation from "./App/Navigations/TabNavigation";
 
 export default function App() {
+  const tokenCache = {
+    async getToken(key) {
+      try {
+        return SecureStore.getItemAsync(key);
+      } catch (err) {
+        return null;
+      }
+    },
+    async saveToken(key, value) {
+      try {
+        return SecureStore.setItemAsync(key, value);
+      } catch (err) {
+        return;
+      }
+    },
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ClerkProvider
+      publishableKey="pk_test_ZmFzdC1saW9uZXNzLTYyLmNsZXJrLmFjY291bnRzLmRldiQ"
+      tokenCache={tokenCache}
+    >
+      <View style={styles.container}>
+        {/* SignIn Component  */}
+        <SignedIn>
+          <NavigationContainer>
+            <TabNavigation />
+          </NavigationContainer>
+        </SignedIn>
+        {/* SignOut Component  */}
+        <SignedOut>
+          <Login />
+        </SignedOut>
+        <StatusBar style="auto" />
+      </View>
+    </ClerkProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: Colors.White,
   },
 });
---
+++ App/Utils/Colors.js
@@ -0,0 +1,6 @@
+export default {
+  Dark: '#121212',
+  Black: '#313638',     // Dark Charchoal   
+  White: '#F8F7F4',     // Cold Steel
+  Primary: '#8E3FFF',
+}
